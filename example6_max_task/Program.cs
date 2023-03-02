28cf4a9b8d769e5823ac97e61495ef3a30350a7a
Юлия Гришко <dimondgyv30@gmail.com>
2023-03-02 18:31:44 +0500

    added max task

---
+++ example6_max_task/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 3;
+int b = 54;
+int c = 7;
+int d = 13;
+int e = 77;
+
+int max = a;
+
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+Console.Write("max = ");
+Console.Write(max);
