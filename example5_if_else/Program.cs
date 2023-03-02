a385fcca686405c449f2a3a69f11949ee42a8f9d
Юлия Гришко <dimondgyv30@gmail.com>
2023-03-02 18:16:57 +0500

    added if else task

---
+++ example5_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your name!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{ 
+    Console.WriteLine("Have a nice day, Masha!");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
+
